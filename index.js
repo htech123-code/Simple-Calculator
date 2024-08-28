c22022e337ba275a9b5931e59c4eec01368a2910
htech123-code <htechlearning1@gmail.com>
2024-08-28 10:27:04 -0700

    simple calculator project

---
+++ index.js
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{
+        message: "Enter your First Number",
+        type: "number",
+        name: "firstNumber"
+    }, {
+        message: "Enter your Second Number",
+        type: "number",
+        name: "secondNumber"
+    }, {
+        message: "Select One of the operators to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"]
+    }]);
+// console.log(answer)
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Select the correct operator");
+}
